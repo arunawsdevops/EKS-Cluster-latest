03feddb1685726a9ee0f45d43b1b6536beba8824
remyars18 <remyars009@gmail.com>
2025-03-12 22:38:21 +0530

    remove

--- EKS-Cluster-latest/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "eks-cluster-terra-state-secondary"
-    key    = "terraform/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "eks-cluster-terra-state-secondary"
-    key    = "terraform/terraform.tfstate"
-    region = "us-west-2c"
-  }
-}
